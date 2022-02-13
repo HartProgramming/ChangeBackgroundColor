ee0f205959a135646b37306d1027a4aaa8635078
Web3Learner <programmedhart@protonmail.com>
2022-02-13 17:17:39 -0500

    commit

--- BackgroundColorChange.js
+++ BackgroundColorChange.js
@@ -11,6 +11,8 @@ function random(x){
     return Math.floor(Math.random()* x)
 }
 
+/* Button function that returns the color of the array
+at a random position and attaches it to the body and button */
 button.addEventListener("click", function(){
     let bodyColor = body.style.backgroundColor = colorArray[random(7)];
     button.style.backgroundColor = bodyColor;
